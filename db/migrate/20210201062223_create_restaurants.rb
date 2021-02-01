3381a6a3b0b6d1eba621f9e9d5860c0304ec3ddb
andre ferrer <andrerferrer@hotmail.com>
2021-02-01 07:28:01 -0300

    Finish livecode

---
+++ db/migrate/20210201062223_create_restaurants.rb
@@ -0,0 +1,11 @@
+# create a migration file with ruby (AR)
+
+class CreateRestaurants < ActiveRecord::Migration[6.0]
+  def change
+    create_table :restaurants do |table|
+      table.string :name
+      table.string :address
+      table.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,28 @@
-# This is where you can create initial data for your app.
+require 'faker'
+# we write here the same that we would write in the `rake console`
+
+# GOOD STYLE
+puts "CLEAN THE DB"
+Restaurant.destroy_all
+
+# puts "Creating Restaurants"
+
+# # Restaurant.create(name: "Little Yellow", address: "Rio", rating: 5)
+# yellow = Restaurant.new(name: "Little Yellow", address: "Rio", rating: 5)
+# yellow.save
+
+# Restaurant.create(name: "Mc Donalds", address: "Everywhere", rating: 2)
+# Restaurant.create(name: "Burger King", address: "Everywhere", rating: 2)
+
+# puts "We are done. Enjoy"
+
+puts 'Creating 100 fake restaurants...'
+100.times do
+  restaurant = Restaurant.new(
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    rating:  rand(0..5)
+  )
+  restaurant.save
+end
+puts 'Finished!'
